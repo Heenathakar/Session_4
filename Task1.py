4e6afb011676a16220980753de1c23816613617d
Heenathakar <hinathakar05@gmail.com>
2026-06-22 11:06:58 +0530

    first commit

---
+++ Task1.py
@@ -0,0 +1,10 @@
+    #Take the string 'Flipkart-Sale2024' and use string methods to convert it to lowercase, 
+#replace the dash with a space, and print the result.
+
+#Task - 1
+
+s="Flipkart-Sale2024"
+print(len(s))
+print(s.casefold())
+print(s.replace("-","space"))
+print(s.center(50,"#"))
---
+++ Task4.py
@@ -0,0 +1,9 @@
+#Given the string 'Spotify_Premium_Offer', use string slicing to extract and print only the word 'Premium'.
+#S p o t i f y _ P r e   m  i u  m  _  O  f  f  e  r
+#0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+#Task - 4
+
+st="Spotify_Premium_Offer"
+
+print(len(st))
+print(st[8:15:1])
