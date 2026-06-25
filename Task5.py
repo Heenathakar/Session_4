d216cdda9e3417efbb3b251735605d67a6ae0b39
Heenathakar <147593132+Heenathakar@users.noreply.github.com>
2026-06-25 18:47:53 +0530

    Add files via upload

---
+++ Task5.py
@@ -0,0 +1,7 @@
+# Format and print a message using variables: product = 'Myntra Shirt', price = 799.5. The output should be: 'Deal: Myntra Shirt is available at ₹799.50 only!'
+# using string formatting. answer of this 
+
+product = 'Myntra Shirt'
+price = 799.5
+
+print("Deal: {product} is available at ₹{price:.2} only!")
